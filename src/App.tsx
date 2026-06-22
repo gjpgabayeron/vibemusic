67b56620da3663d3b0bd0f4211d9cea6fa15fb73
gjpgabayeron <gjpgabayeron@gmail.com>
2026-06-22 16:51:38 +0800

    chore: add eslint rule

--- src/App.tsx
+++ src/App.tsx
@@ -158,6 +158,7 @@ export default function App() {
         });
       }
     }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [isSettingsLoading, activeProfileId, fetchLibrary]);
 
   useWindowCloseHandler(() => setIsQuitDialogOpen(true));
